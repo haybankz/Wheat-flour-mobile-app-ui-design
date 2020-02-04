82a33cd2f0809d6861522c7a2910e6828c7e8989
Oladele Abeeb <sunsyde2020@gmail.com>
2020-02-04 19:32:54 +0100

    set up flutter

---
+++ android/app/src/main/kotlin/com/example/wheat_flour_mobile_app_ui_design/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wheat_flour_mobile_app_ui_design
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
